979dff60b5b6e9449bf490eb800788e4e1a42f35
LampLure <134139200+LampLure@users.noreply.github.com>
2026-05-19 19:20:52 +0800

    Load CJK fonts for egui text rendering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,80 @@
 use ai_video::ui::app::AiVideoApp;
 use eframe::egui;
+use std::fs;
+use std::path::Path;
+
+fn find_cjk_font() -> Option<Vec<u8>> {
+    let linux_fonts = [
+        "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc",
+        "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.otf",
+        "/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc",
+        "/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.otf",
+        "/usr/share/fonts/noto-cjk/NotoSansCJK-Regular.ttc",
+        "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc",
+        "/usr/share/fonts/wqy-microhei/wqy-microhei.ttc",
+        "/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf",
+        "/usr/share/fonts/truetype/arphic/uming.ttc",
+    ];
+    for path in &linux_fonts {
+        if let Ok(data) = fs::read(Path::new(path)) {
+            return Some(data);
+        }
+    }
+
+    let win_fonts = [
+        r"C:\Windows\Fonts\msyh.ttc",
+        r"C:\Windows\Fonts\msyhbd.ttc",
+        r"C:\Windows\Fonts\simhei.ttf",
+        r"C:\Windows\Fonts\simsun.ttc",
+        "/mnt/c/Windows/Fonts/msyh.ttc",
+        "/mnt/c/Windows/Fonts/msyhbd.ttc",
+        "/mnt/c/Windows/Fonts/simhei.ttf",
+        "/mnt/c/Windows/Fonts/simsun.ttc",
+    ];
+    for path in &win_fonts {
+        if let Ok(data) = fs::read(Path::new(path)) {
+            return Some(data);
+        }
+    }
+
+    let macos_fonts = [
+        "/System/Library/Fonts/PingFang.ttc",
+        "/System/Library/Fonts/STHeiti Light.ttc",
+        "/System/Library/Fonts/Hiragino Sans GB.ttc",
+        "/Library/Fonts/Noto Sans CJK TC Regular.otf",
+    ];
+    for path in &macos_fonts {
+        if let Ok(data) = fs::read(Path::new(path)) {
+            return Some(data);
+        }
+    }
+
+    None
+}
+
+fn install_cjk_font(ctx: &egui::Context) {
+    let Some(cjk_font_data) = find_cjk_font() else {
+        eprintln!("No CJK font found. Install fonts-noto-cjk or fonts-wqy-microhei if Chinese text renders as squares.");
+        return;
+    };
+
+    let mut fonts = egui::FontDefinitions::default();
+    fonts.font_data.insert(
+        "cjk".to_owned(),
+        egui::FontData::from_owned(cjk_font_data).into(),
+    );
+    fonts
+        .families
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "cjk".to_owned());
+    fonts
+        .families
+        .entry(egui::FontFamily::Monospace)
+        .or_default()
+        .insert(0, "cjk".to_owned());
+    ctx.set_fonts(fonts);
+}
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
@@ -12,6 +87,9 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "AI Video",
         options,
-        Box::new(|_cc| Ok(Box::new(AiVideoApp::default()))),
+        Box::new(|cc| {
+            install_cjk_font(&cc.egui_ctx);
+            Ok(Box::new(AiVideoApp::default()))
+        }),
     )
 }
